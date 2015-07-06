edf41958f5cffed2066c8af088e5851435040075
aoran <32862082@qq.com>
2015-07-07 00:07:24 +0800

    commit

---
+++ ScrollViewAutolayoutDemo/ScrollViewAutolayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewAutolayoutDemo
+//
+//  Created by 敖然 on 15/7/6.
+//  Copyright (c) 2015年 AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewAutolayoutDemo/ScrollViewAutolayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewAutolayoutDemo
+//
+//  Created by 敖然 on 15/7/6.
+//  Copyright (c) 2015年 AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
